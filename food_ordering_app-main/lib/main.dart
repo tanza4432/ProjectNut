5a24b88af5a923e40f99dd0af2b918ef0a9beaa8
ศุภณัฐ ชินราช <guitar@odds.team>
2022-02-27 18:21:44 +0700

    clean

--- food_ordering_app-main/lib/main.dart
+++ food_ordering_app-main/lib/main.dart
@@ -1,22 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:restaurant/dashboard.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       theme: ThemeData(fontFamily: 'mont'),
-//       home: MyHomePage(),
-//     );
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
 
 import 'screens/signin/Signin.dart';
