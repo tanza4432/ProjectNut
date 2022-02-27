34c5f27c5e66fdc027b9f97fc76f34371d6e5fae
ศุภณัฐ ชินราช <guitar@odds.team>
2022-02-27 12:22:13 +0700

    insert project

--- food_ordering_app-main/lib/main.dart
+++ food_ordering_app-main/lib/main.dart
@@ -1,17 +1,131 @@
-import 'package:flutter/material.dart';
-import 'package:restaurant/dashboard.dart';
+// import 'package:flutter/material.dart';
+// import 'package:restaurant/dashboard.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(fontFamily: 'mont'),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+
+
+import 'package:flutter/material.dart';
+
+import 'screens/signin/Signin.dart';
+import 'theme/Style.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'mont'),
-      home: MyHomePage(),
+      title: 'Auth Screen 1',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: TextTheme(
+          headline4: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          button: TextStyle(color: kPrimaryColor),
+          headline1:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.white.withOpacity(.2),
+            ),
+          ),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("asset/startLogo.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "แอปค้นหาร้านอาหาร\n",
+                      ),
+                      TextSpan(
+                        text: "แอปพลิเคชันเพื่อกรณีศึกษา",
+                      )
+                    ],
+                  ),
+                ),
+                FittedBox(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) {
+                          return SignInScreen();
+                        },
+                      ));
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 25),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 26, vertical: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: kPrimaryColor,
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            "เริ่มต้น",
+                            style: Theme.of(context).textTheme.button.copyWith(
+                                  color: Colors.black,
+                                ),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(
+                            Icons.arrow_forward,
+                            color: Colors.black,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- food_ordering_app-main/lib/theme/Style.dart
+++ food_ordering_app-main/lib/theme/Style.dart
@@ -4,3 +4,5 @@ Color blue = new Color(0xff50c9ff);
 Color green = new Color(0xff00e582);
 Color black = new Color(0xff4d4d4d);
 Color greenBtn = new Color(0xff00d672);
+const kBackgroundColor = Color(0xFF202020);
+const kPrimaryColor = Color(0xFFFFBD73);
